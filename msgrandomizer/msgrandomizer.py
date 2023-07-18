eec7e26c0b567ee89e6b9a2bd1907c576f3e82ba
dot_dash <dotdashgames@gmail.com>
2023-07-17 20:49:34 -0400

    Got basic image embeds functional w/ menu, need to implement system to deal with video/reddit/twitter links

--- msgrandomizer/msgrandomizer.py
+++ msgrandomizer/msgrandomizer.py
@@ -1,6 +1,7 @@
 from redbot.core import commands
 from redbot.core import Config
-import discord, pprint
+from redbot.core import utils
+import discord, random, itertools, pprint
 
 class MsgRandomizer(commands.Cog):
     """Display messages containing media in a random order from a given channel."""
@@ -9,12 +10,24 @@ class MsgRandomizer(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=49502314239500134)
         self.config.register_guild(
-            cachedChannels = {
-                123 : {
-                    312 : ["url1, url2"]
-                }
-            }
+            cachedChannels = {}
         )
+    
+    async def processMessage(self, message, channel, cachedChannels):
+
+        if str(message.id) != cachedChannels[str(channel.id)]["lastCheckedMsg"]:
+            cachedChannels[str(channel.id)].update({"lastCheckedMsg" : str(message.id)})
+
+        if message.embeds or message.attachments:
+            tmpCachedMedia = []
+            for embed, attachment in itertools.zip_longest(message.embeds, message.attachments):
+                if embed is not None:
+                    tmpCachedMedia.append(embed.url)
+                if attachment is not None:
+                    tmpCachedMedia.append(attachment.url)
+            cachedChannels[str(channel.id)].update({str(message.id) : tmpCachedMedia})
+
+        return cachedChannels
 
     @commands.command()
     async def randomize(self, ctx, channel : discord.TextChannel):
@@ -25,31 +38,70 @@ class MsgRandomizer(commands.Cog):
         cachedChannels = await self.config.guild(ctx.guild).cachedChannels()
 
         # Check if the passed channel is cached, if it is, check if there are new URLs to cache
-        if str(channel.id) in cachedChannels.keys():
+        if str(channel.id) in cachedChannels:
             async with ctx.typing():
-                async for message in channel.history(after=list(cachedChannels[channel.id].keys())[-1], limit=None):
-                    if message.embeds:
-                        cachedChannels[str(channel.id)].update({str(message.id) : [embed.url for embed in message.embeds]})
+                async for message in channel.history(after=await channel.fetch_message(list(cachedChannels[str(channel.id)].keys())[-2]), limit=None):
+                    cachedChannels = await self.processMessage(message, channel, cachedChannels)
             await ctx.send("Successfully updated channel cache!")
 
         else:
             # We have not cached this channel yet, go through it from oldest to newest and add all the messages with embeds to the dict of cached channels
             async with ctx.typing():
-                cachedChannels.update({str(channel.id) : {}})
+                cachedChannels.update({str(channel.id) : {"lastCheckedMsg" : ""}})
 
                 async for message in channel.history(oldest_first=True, limit=None):
-                    if message.embeds:
-                        cachedChannels[str(channel.id)].update({str(message.id) : [embed.url for embed in message.embeds]})
+                    cachedChannels = await self.processMessage(message, channel, cachedChannels)
             await ctx.send("Successfully added channel to cached channels list!")
         
+        # Update the cache with our new data if it exists (if not, no problem in just setting the dict to its existing value)
         await self.config.guild(ctx.guild).cachedChannels.set(cachedChannels)
 
+        # Form our list of embeds and randomize them
+        mediaEmbeds = []
+
+        for messageId, messageData in cachedChannels[str(channel.id)].items():
+            if messageId == "lastCheckedMsg":
+                continue
+            for data in messageData:
+                embed = discord.Embed()
+                print(f"Data: {data}")
+                if "jpg" in data or "png" in data:
+                    print("Interpreting as image")
+                    embed.set_image(url=data)
+                    embed.description = "Test"
+                    embed.title = "Test"
+                    mediaEmbeds.append(embed)
+                elif "youtube" in data:
+                    print("Interpreting as video")
+                    embed.video.url = data
+                    embed.description = "Test"
+                    embed.title = "Test"
+                    mediaEmbeds.append(embed)
+                else:
+                    print(f"Unknown type")
+                    embed.url = data
+                    embed.description = "Test"
+                    embed.title = "Test"
+                    mediaEmbeds.append(embed)
+
+        # Create a menu of the embeds and display it
+        await utils.menus.menu(ctx, mediaEmbeds)
+
     @commands.command()
-    async def displaycached(self, ctx):
+    async def showcached(self, ctx):
         cachedChannels = await self.config.guild(ctx.guild).cachedChannels()
 
-        await ctx.send(pprint.pformat(cachedChannels))
+        pprint.pprint(cachedChannels)
 
     @commands.command()
     async def clearcached(self, ctx):
         await self.config.clear_all_guilds()
+        await ctx.send("Cache cleared from all guilds.")
+
+    @commands.command()
+    async def test(self, ctx):
+        """
+        msg = await [channel for channel in ctx.guild.channels if channel.name == "general"][0].fetch_message(1130604776988618937)
+        await ctx.send(msg.attachments)
+        """
+        return
